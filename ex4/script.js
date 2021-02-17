c10ad637cb486de1c2e5c17d04062be534194c64
bnaviarch <123789+bnaviarch@users.noreply.github.com>
2021-02-16 21:10:12 -0500

    fix

---
+++ ex4/script.js
@@ -0,0 +1,10 @@
+let age = 28;
+let height =7;
+
+if(age<8) {
+  console.log("Check out the merry-go-round; you'll love it!");
+} else if (age >= 8 && age < 65 && height > 4.5) {
+  console.log("Check out the roller-coaster!");
+} else {
+  console.log("Check out the lazy-river.");
+}
