f0a6ec2e00fd5825b1f8046d3aa8a6a32e70bb8b
bnaviarch <123789+bnaviarch@users.noreply.github.com>
2021-02-23 21:09:51 -0500

    Update ex4script.js

--- ex4/ex4script.js
+++ ex4/ex4script.js
@@ -9,7 +9,7 @@ if(age<8) {
   console.log("Check out the lazy-river.");
 }
 
-/*
+
 
 let nouns = ['doggo', 'lemur', 'banana', 'Chaka Khan & Rufus', 'Hipster Store in BK that sells both bicycles and candles']
 let verbs = ['leaps', 'attacks', 'walks', 'runs', 'smells']
@@ -30,16 +30,18 @@ for (i = 0; i < adjectives.length; i++) {
 var sentence = `My ${noun} leaps ${adjective} when I ${verb} a rainbow in the sky.`
 console.log(sentence)
 
-*/
+/*
 
 let nouns = ['doggo', 'lemur', 'banana', 'Chaka Khan & Rufus', 'Hipster Store in BK that sells both bicycles and candles']
 let verbs = ['leaps', 'attacks', 'walks', 'runs', 'smells']
 let adjectives = ['powerfully', 'promiscuously', 'happily', 'sadly', 'thunderously']
 
 let randomnoun =
-let randomverb = 
+let randomverb =
 let randomadjective =
 
 console.log(
 var sentence = `My ${randomnoun} leaps ${randomadjective} when I ${randomverb} a rainbow in the sky.`
 );
+
+*/
