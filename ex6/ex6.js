9e463ce57f3e3fbc695f30998517d7c8eaa6ae58
bnaviarch <123789+bnaviarch@users.noreply.github.com>
2021-03-09 19:08:24 -0500

    new ex6

---
+++ ex6/ex6.js
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>js exercises 01</title>
+  </head>
+  <body>
+    <script src="ex6script.js" ></script>
+  </body>
+</html>
