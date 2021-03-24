3bc99e26a195c530b739be88aa72157985c5649d
bnaviarch <123789+bnaviarch@users.noreply.github.com>
2021-03-23 20:39:10 -0400

    linked

--- ex9/books.js
+++ ex9/books.js
@@ -1,8 +1,8 @@
+console.log('this works.')
+
 const GOOGLE_BOOKS_URL =
   "https://www.googleapis.com/books/v1/volumes?q=isbn:0679775439";
 
-  fetch('http://example.com/movies.json')
+  fetch(GOOGLE_BOOKS_URL)
     .then(response => response.json())
     .then(data => console.log(data));
-
-    
